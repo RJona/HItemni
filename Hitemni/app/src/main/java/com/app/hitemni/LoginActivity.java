c7a93db3ca558f2a1e67e4429e0f57d61c6efa0d
Arthur Jamain <a.jamain.pro@gmail.com>
2015-04-08 23:26:56 +0200

    Clean + modif facebook

--- Hitemni/app/src/main/java/com/app/hitemni/LoginActivity.java
+++ Hitemni/app/src/main/java/com/app/hitemni/LoginActivity.java
@@ -1,14 +1,23 @@
 package com.app.hitemni;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
+
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.model.GraphUser;
+import com.facebook.widget.LoginButton;
+
+import java.util.Arrays;
 
 /**
  * Created by OptiFine on 08/04/2015.
@@ -18,6 +27,9 @@ public class LoginActivity extends Activity {
     private EditText  username=null;
     private EditText  password=null;
     private Button login;
+    private LoginButton loginBtn;
+    private UiLifecycleHelper uiHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +37,71 @@ public class LoginActivity extends Activity {
         username = (EditText)findViewById(R.id.editText1);
         password = (EditText)findViewById(R.id.editText2);
         login = (Button)findViewById(R.id.button1);
+
+        uiHelper = new UiLifecycleHelper(this, statusCallback);
+        uiHelper.onCreate(savedInstanceState);
+
+
+        loginBtn = (LoginButton) findViewById(R.id.login_button);
+        loginBtn.setReadPermissions(Arrays.asList("email"));
+        loginBtn.setUserInfoChangedCallback(new LoginButton.UserInfoChangedCallback() {
+            @Override
+            public void onUserInfoFetched(GraphUser graphUser) {
+                if(graphUser != null) {
+                    Toast.makeText(getApplicationContext(), "You are currently connected as "+graphUser.getName(),
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    } 
+
+    private Session.StatusCallback statusCallback = new Session.StatusCallback() {
+        @Override
+        public void call(Session session, SessionState sessionState, Exception e) {
+            if (sessionState.isOpened()) {
+                Log.d("MainActivity", "Facebook session opened.");
+                Toast.makeText(getApplicationContext(), "Facebook session opened.",
+                        Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), AccueilActivity.class);
+                startActivity(intent);
+            } else if (sessionState.isClosed()) {
+                Log.d("MainActivity", "Facebook session closed.");
+                Toast.makeText(getApplicationContext(), "Facebook session closed.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        uiHelper.onResume();
     }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        uiHelper.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        uiHelper.onDestroy();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        uiHelper.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedState) {
+        super.onSaveInstanceState(savedState);
+        uiHelper.onSaveInstanceState(savedState);
+    }
+
 
     public void login(View view){
         if(username.getText().toString().equals("Admin") &&
@@ -38,7 +114,6 @@ public class LoginActivity extends Activity {
         else{
             Toast.makeText(getApplicationContext(), "Wrong Credentials",
                     Toast.LENGTH_SHORT).show();
-
         }
 
     }
@@ -48,7 +123,7 @@ public class LoginActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.main, menu);
+        //getMenuInflater().inflate(R.menu., menu);
         return true;
     }
 }
